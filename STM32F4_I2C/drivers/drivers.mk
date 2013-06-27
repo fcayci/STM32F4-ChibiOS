f6213c086572b9bdaa5770112ebc153e6227800a
Furkan Cayci <furkanca02@gmail.com>
2013-06-27 12:00:50 -0400

    changed stm32f4-i2c to work with mpu6050

---
+++ STM32F4_I2C/drivers/drivers.mk
@@ -0,0 +1,7 @@
+# List of all the board related files.
+DRIVERSRC = drivers/src/i2cdev_chibi.c \
+			drivers/src/mpu6050.c \
+			drivers/src/mpu60x0.c
+
+# Required include directories
+DRIVERINC = drivers/inc/

540cc11b15aca7cf2287d3c76bef88427546a2d0
Furkan Cayci <furkanca02@gmail.com>
2014-05-29 23:45:10 -0400

    Serial example modifications and corrections

--- serial/main.c
+++ serial/main.c
@@ -1,6 +1,7 @@
 /*
  * A Simple USART example for printing on the serial port
- * Connect RX/TX to C10/C11
+ * PC10 - TX (transmits data from uC).
+ * PC11 - RX
  */
 
 #include "ch.h"
@@ -9,7 +10,7 @@
 #include "chprintf.h"
 
 #define USART_CR1_9BIT_WORD		(1 << 12) /* CR1 9 bit word */
-#define USART_CR1_PARITY_SET 	(1 << 10) /* CR1 Parity bit enable */
+#define USART_CR1_PARITY_SET 	(1 << 10) /* CR1 parity bit enable */
 #define USART_CR1_EVEN_PARITY	(0 << 9)  /* CR1 even parity */
 
 #define PrintS2(x)    SendString(&SD3, x);	/* for example 4 */
@@ -18,7 +19,7 @@ static void print(char *p);
 static void println(char *p);
 static void printn(uint32_t n);
 
-static SerialConfig sd2cfg = {
+static SerialConfig sd3cfg = {
    115200,									/* 115200 baud rate */
    USART_CR1_9BIT_WORD | USART_CR1_PARITY_SET | USART_CR1_EVEN_PARITY,
    USART_CR2_STOP1_BITS | USART_CR2_LINEN,
@@ -31,29 +32,30 @@ int main(void) {
 	chSysInit();
 
 	/*
-	* Activates the serial driver 2 using the driver sd2cfg configuration.
-	* PA2(TX) and PA3(RX) are routed to USART2.
+	* Activates the serial driver 3 using the driver sd3cfg configuration.
+	* PC10(TX) and PC11(RX) are routed to USART3.
 	* PAL_MODE_ALTERNATE is the value that you pass from Table 8. Alternate function mapping
 	* in DM00037051 - STM32F405xx/STM32F407xx Datasheet
 	*/
-	sdStart(&SD3, &sd2cfg);
-	// sdStart(&SD2, NULL); /* for default configuration */
+	sdStart(&SD3, &sd3cfg);
 	palSetPadMode(GPIOC, 10, PAL_MODE_ALTERNATE(7));
 	palSetPadMode(GPIOC, 11, PAL_MODE_ALTERNATE(7));
 
 	while (TRUE){
-		static uint8_t temp = 7;
+		static uint8_t temp = 0;
 
 	    if (palReadPad(GPIOA, GPIOA_BUTTON))
-	    	sdWrite(&SD3, (uint8_t *)"Button Pressed!\r\n", 17);
+            sdWrite(&SD3, (uint8_t *)"Button Pressed!\r\n", 17);
 	    sdWrite(&SD3, (uint8_t *)"Example: 1\r\n", 12);
 		chprintf((BaseSequentialStream *)&SD3, "Example: %d\r\n", 2);
 		SendString(&SD3, "Example: 3\r\n");
 		PrintS2("Example: 4\r\n");
 		print("Example: 5\r\n");
-		println("Example 6");
-		print("Example: "); printn(temp); print("\r\n");
-		chThdSleepMilliseconds(500); /* Sleep the processor for 500 milliseconds */
+		println("Example: 6");
+		temp = temp + 1;
+		print("Counter: "); printn(temp); print("\r\n++++++++++++\r\n");
+		if (temp == 255) temp = 0;
+		chThdSleepMilliseconds(250); /* Sleep the processor for 250 milliseconds */
 	}
 }
 
